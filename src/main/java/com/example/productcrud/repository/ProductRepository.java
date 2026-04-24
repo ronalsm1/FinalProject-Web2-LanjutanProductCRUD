39edb054fb0dfe462735975c1489c48dafe2e1cd
brandondavids <bdsinambela@gmail.com>
2026-04-24 20:01:03 +0700

    [add]: search & filter and pagination implemented

--- src/main/java/com/example/productcrud/repository/ProductRepository.java
+++ src/main/java/com/example/productcrud/repository/ProductRepository.java
@@ -8,10 +8,19 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+// Import pageable ~Brandon David
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findByOwner(User owner);
 
     Optional<Product> findByIdAndOwner(Long id, User owner);
+
+    // Menggunakan Page dan membuat function: ~Brandon David
+    // findByOwnerAndNameContainingIgnoreCase & findByOwnerAndNameContainingIgnoreCaseAndCategory_Id
+    Page<Product> findByOwnerAndNameContainingIgnoreCase(User owner, String keyword, Pageable pageable);
+    Page<Product> findByOwnerAndNameContainingIgnoreCaseAndCategory_Id(User owner, String keyword, Long categoryId, Pageable pageable);
 }
